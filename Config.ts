23889d6930a0fd79a65ab74b2c64b4f80121cc5f
Magalhães de Lemos <66646459+MexLemos@users.noreply.github.com>
2025-01-17 12:30:54 +0100

    teste

---
+++ Config.ts
@@ -0,0 +1,19 @@
+import multer  from "multer";
+import path from "path"
+
+
+export const storage = multer.diskStorage({
+   destination:(req, File, callbackss) =>{
+        callback(null, path.resolve("Uploads"));
+   },
+   filename: (req, file, callback) =>{
+    const time = new Date().getTime();
+
+
+
+
+    callback(null, `${time}_${file.originalname}`)
+   },
+   
+
+})
